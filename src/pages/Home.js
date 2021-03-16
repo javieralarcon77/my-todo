0c3221029ab020945a3c3ed3cb8bea4921bc3f42
Javier Alarcon <alarcon.vt@gmail.com>
2021-03-16 08:52:22 -0500

    agregada estructura para home page

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/organisms/Layout'
+
+const Home = () => {
+    return (
+        <Layout title="Home">
+
+        </Layout>
+    )
+}
+
+export default Home
